5e3598f560705a4745bf7f412a4105fd24283fd0
Mark Talbot <mark.talbot@experiencepoint.com>
2017-11-03 13:51:51 -0400

    Minor clean-up

--- config/mocha-test-shim.js
+++ config/mocha-test-shim.js
@@ -1,17 +1,9 @@
 var jsdom = require('jsdom');
-
 const { JSDOM } = jsdom;
-// console.log('------------------------------------------')
-// console.log(jsdom)
-// console.log('------------------------------------------')
-
-// var document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 var dom = new JSDOM('<!doctype html><html><body></body></html>');
 
-// var window = document.defaultView;
 var window = dom.window;
 
-// global.document = document;
 global.document = window.document;
 global.HTMLElement = window.HTMLElement;
 global.XMLHttpRequest = window.XMLHttpRequest;
